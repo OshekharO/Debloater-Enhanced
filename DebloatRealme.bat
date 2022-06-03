0e8c06d20752f014ce29ea54c69b72b34f73f63b
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-06-03 08:39:11 +0530

    Update DebloatRealme.bat

--- DebloatRealme.bat
+++ DebloatRealme.bat
@@ -12,11 +12,25 @@ pause
 adb wait-for-any-device
 adb reconnect
 adb wait-for-any-device
-adb shell pm uninstall -k --user 0 cn.wps.moffice_eng
-adb shell pm uninstall -k --user 0 com.google.android.apps.podcasts
-adb shell pm uninstall -k --user 0 com.realme.link
-adb shell pm uninstall -k --user 0 com.google.android.apps.youtube.music
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall cn.wps.moffice_eng
+adb shell pm uninstall com.google.android.apps.podcasts
+adb shell pm uninstall com.realme.link
+adb shell pm uninstall com.google.android.apps.youtube.music
+adb shell pm uninstall com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.okgoogle
+adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.files
+adb shell pm uninstall -k --user 0 com.google.android.printservice.recommendation
+adb shell pm uninstall -k --user 0 com.google.ar.core
+adb shell pm uninstall -k --user 0 com.google.android.apps.googleassistant
+adb shell pm uninstall -k --user 0 com.google.ar.lens
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+adb shell pm uninstall -k --user 0 com.google.android.keep
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+adb shell pm uninstall -k --user 0 com.google.android.projection.gearhead
+adb shell pm uninstall -k --user 0 com.google.android.feedback 
+adb shell pm uninstall -k --user 0 com.coloros.childrenspace
+adb shell pm uninstall -k --user 0 com.coloros.compass2
+adb shell pm uninstall -k --user 0 com.coloros.logkit 
 adb shell pm uninstall -k --user 0 com.coloros.systemclone
 adb shell pm uninstall -k --user 0 com.realmepay.payments
 adb shell pm uninstall -k --user 0 com.opos.cs
@@ -47,15 +61,15 @@ adb shell pm uninstall -k --user 0 com.coloros.video
 adb shell pm uninstall -k --user 0 com.heytap.browser
 adb shell pm uninstall -k --user 0 com.os.docvault
 adb shell pm uninstall -k --user 0 com.finshell.fin
-adb shell pm uninstall -k --user 0 com.coloros.onekeylockscreen
-adb shell pm uninstall -k --user 0 com.realmecomm.app
-adb shell pm uninstall -k --user 0 com.heytap.quickgame
+adb shell pm uninstall com.coloros.onekeylockscreen
+adb shell pm uninstall com.realmecomm.app
+adb shell pm uninstall com.heytap.quickgame
 adb shell pm uninstall -k --user 0 com.heytap.cloud
 adb shell pm uninstall -k --user 0 com.heytap.usercenter
-adb shell pm uninstall -k --user 0 com.realmestore.app
-adb shell pm uninstall -k --user 0 com.google.android.videos
-adb shell pm uninstall -k --user 0 com.redteamobile.roaming
 adb shell pm uninstall -k --user 0 com.oplus.games
+adb shell pm uninstall com.realmestore.app
+adb shell pm uninstall com.google.android.videos
+adb shell pm uninstall -k --user 0 com.redteamobile.roaming
 adb shell pm disable-user com.android.fmradio
 adb shell pm disable-user com.nearme.statistics.rom
 adb shell pm disable-user com.glance.internet
